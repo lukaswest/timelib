4615458247f18ac9bc9ce3b5026b2651c753b15f
Lukas Westerfeld <lukas.westerfeld@posteo.de>
2018-12-23 01:13:49 +0100

    first version of timelib.

--- main.c
+++ main.c
@@ -11,9 +11,52 @@
  *
  *
  **/
+
+ int exists_date(int day, int month, int year){
+    if ((year+month+day) <1584){
+        return 0;
+    }
+    else if ((year+month+day) >2443){
+        return 0;
+    }
+    else {
+        return 1;
+    }
+ }
+
+int get_Days_for_month(int month, int year){
+    int Count[ ]={31,28,31,30,31,30,31,31,30,31,30,31};
+    return Count[month-1];
+}
+
+int is_leapyear(int year){
+    if ((year%4)==0){
+        if ((year%100)!=0){
+            return 1;
+        }
+        else if((year%400)==0){
+                return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    else {
+        return 0;
+    }
+    if (year<1582){return -1;}
+}
+
+
 int day_of_the_year(int day, int month, int year)
 {
+    int leapyear= is_leapyear(year);
+    int exists =exists_date(day,month,year);
+    int i=0, Anzahl=0;
 
+    while (i<month){
+        Anzahl+=get_Days_for_month(month, year);
+    }
     return 10;
 }
 
