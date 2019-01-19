e7fe5dcd68fd67b98782c5979d108e29da3ae2e3
Lukas Westerfeld <lukaswesterfeld@posteo.de>
2019-01-20 00:00:25 +0100

    update main.c

--- main.c
+++ main.c
@@ -24,11 +24,6 @@
     }
  }
 
-int get_Days_for_month(int month, int year){
-    int Count[ ]={31,28,31,30,31,30,31,31,30,31,30,31};
-    return Count[month-1];
-}
-
 int is_leapyear(int year){
     if ((year%4)==0){
         if ((year%100)!=0){
@@ -44,24 +39,37 @@ int is_leapyear(int year){
     else {
         return 0;
     }
-    if (year<1582){return -1;}
+}
+int get_Days_for_month(int month, int year){
+    int leapyear= is_leapyear(year);
+    int Count[ ]={31,28,31,30,31,30,31,31,30,31,30,31};
+    if(leapyear==1){
+        Count[1]=29;
+    }
+    return Count[month-1];
 }
 
 
 int day_of_the_year(int day, int month, int year)
 {
-    int leapyear= is_leapyear(year);
     int exists =exists_date(day,month,year);
-    int i=0, Anzahl=0;
-
-    while (i<month){
-        Anzahl+=get_Days_for_month(month, year);
+    int i=1, Anzahl=0;
+    if (exists==1){
+        while (i<month){
+            Anzahl+=get_Days_for_month(i, year);
+            i++;
+        }
+        Anzahl+= day;
+        return Anzahl;
     }
-    return 10;
+    if (exists==0){
+        return -1;
+    }
+
 }
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(18, 12, 2018));
+    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
     return 0;
 }
